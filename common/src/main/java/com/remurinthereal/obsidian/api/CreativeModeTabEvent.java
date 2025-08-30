8ca373f338bb575b3757d89d1cde11633049d66f
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-08-30 01:08:16 -0400

    Clean CreativeModeTabEvent

--- common/src/main/java/com/remurinthereal/obsidian/api/CreativeModeTabEvent.java
+++ common/src/main/java/com/remurinthereal/obsidian/api/CreativeModeTabEvent.java
@@ -1,12 +1,10 @@
 package com.remurinthereal.obsidian.api;
 
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public interface CreativeModeTabEvent {
-    public void add(ItemStack... entries);
-    public void addAfter(ItemStack targetEntry, ItemStack... entries);
-    public void addBefore(ItemStack targetEntry, ItemStack... entries);
-
-    public void remove(ItemStack... entries);
+    void add(ItemStack... entries);
+    void addAfter(ItemStack targetEntry, ItemStack... entries);
+    void addBefore(ItemStack targetEntry, ItemStack... entries);
+    void remove(ItemStack... entries);
 }
