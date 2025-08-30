3f1a64401c86d6bd565b91444b05e3223daa11ab
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-08-30 01:07:35 -0400

    Add getKey to RegistrationSupplier

--- common/src/main/java/com/remurinthereal/obsidian/api/RegistrationSupplier.java
+++ common/src/main/java/com/remurinthereal/obsidian/api/RegistrationSupplier.java
@@ -1,6 +1,9 @@
 package com.remurinthereal.obsidian.api;
 
+import net.minecraft.resources.ResourceKey;
+
 import java.util.function.Supplier;
 
 public interface RegistrationSupplier<T> extends Supplier<T> {
+    ResourceKey<? extends T> getKey();
 }
