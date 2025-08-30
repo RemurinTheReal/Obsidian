6aebcec5f0609bb93a4424380b254814a5c0d1ad
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-08-30 00:47:34 -0400

    Fix modify mistype

--- common/src/main/java/com/remurinthereal/obsidian/platform/CreativeModeTabHelper.java
+++ common/src/main/java/com/remurinthereal/obsidian/platform/CreativeModeTabHelper.java
@@ -1,5 +1,6 @@
 package com.remurinthereal.obsidian.platform;
 
+import com.remurinthereal.obsidian.api.CreativeModeTabEvent;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
@@ -8,7 +9,7 @@ import java.util.function.Consumer;
 
 public final class CreativeModeTabHelper {
     @ExpectPlatform
-    public static void modify(ResourceKey<CreativeModeTab> resourceKey, Consumer<CreativeModeTab> consumer) {
+    public static void modify(ResourceKey<CreativeModeTab> resourceKey, Consumer<CreativeModeTabEvent> consumer) {
         throw new AssertionError();
     }
 }
