ca6d413dc838a65f0d071e3851ef641a8481360a
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-09-01 16:11:12 -0400

    Enable Operator Utils in Dev

---
+++ common/src/main/java/com/remurinthereal/obsidian/mixin/InventoryScreenMixin.java
@@ -0,0 +1,21 @@
+package com.remurinthereal.obsidian.mixin;
+
+import com.remurinthereal.obsidian.platform.PlatformHelper;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(InventoryScreen.class)
+public class InventoryScreenMixin {
+    @Redirect(method = "containerTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/OptionInstance;get()Ljava/lang/Object;"))
+    private Object containerTick(OptionInstance<Object> instance) {
+        return PlatformHelper.isDev() || (boolean)instance.get();
+    }
+
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/OptionInstance;get()Ljava/lang/Object;"))
+    private Object init(OptionInstance<Object> instance) {
+        return PlatformHelper.isDev() || (boolean)instance.get();
+    }
+}
