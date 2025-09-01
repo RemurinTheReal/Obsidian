431efd1c8c3f74e85c92a0f86232f788bb2977e5
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-09-01 16:30:09 -0400

    Update Obsidian.java

--- common/src/main/java/com/remurinthereal/obsidian/Obsidian.java
+++ common/src/main/java/com/remurinthereal/obsidian/Obsidian.java
@@ -1,10 +1,16 @@
 package com.remurinthereal.obsidian;
 
 
+import net.minecraft.resources.ResourceLocation;
+
 public final class Obsidian {
     public static final String MOD_ID = "obsidian";
 
     public static void init() {
 
+    }
+
+    public static ResourceLocation makeResourceLocation(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 }
