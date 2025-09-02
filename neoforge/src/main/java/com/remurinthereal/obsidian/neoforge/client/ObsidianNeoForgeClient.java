9c0b3c777fbe891c3e0c9573adbb2c471c0812b2
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-09-02 14:29:04 -0400

    Remove Unused Imports

--- neoforge/src/main/java/com/remurinthereal/obsidian/neoforge/client/ObsidianNeoForgeClient.java
+++ neoforge/src/main/java/com/remurinthereal/obsidian/neoforge/client/ObsidianNeoForgeClient.java
@@ -2,11 +2,9 @@ package com.remurinthereal.obsidian.neoforge.client;
 
 import com.remurinthereal.obsidian.Obsidian;
 import com.remurinthereal.obsidian.client.ObsidianClient;
-import com.remurinthereal.obsidian.neoforge.ObsidianConfig;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
