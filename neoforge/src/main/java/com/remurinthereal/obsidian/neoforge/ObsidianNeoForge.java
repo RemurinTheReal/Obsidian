545a028a627849b250c6e71213084d5119bffba2
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-09-02 14:28:58 -0400

    Config

--- neoforge/src/main/java/com/remurinthereal/obsidian/neoforge/ObsidianNeoForge.java
+++ neoforge/src/main/java/com/remurinthereal/obsidian/neoforge/ObsidianNeoForge.java
@@ -1,18 +1,21 @@
 package com.remurinthereal.obsidian.neoforge;
 
-import com.remurinthereal.obsidian.platform.neoforge.CreativeModeTabHelperImpl;
-import com.remurinthereal.obsidian.platform.neoforge.RegistrationHelperImpl;
+import com.remurinthereal.obsidian.api.platform.neoforge.CreativeModeTabHelperImpl;
+import com.remurinthereal.obsidian.api.platform.neoforge.RegistrationHelperImpl;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 import com.remurinthereal.obsidian.Obsidian;
-import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.fml.config.ModConfig;
 
 @Mod(Obsidian.MOD_ID)
 public final class ObsidianNeoForge {
-    public ObsidianNeoForge(IEventBus eventBus) {
+    public ObsidianNeoForge(ModContainer modContainer, IEventBus eventBus) {
         eventBus.register(CreativeModeTabHelperImpl.class);
         Obsidian.init();
+
+        modContainer.registerConfig(ModConfig.Type.CLIENT, ObsidianConfig.CLIENT_CONFIG);
 
         RegistrationHelperImpl.registerAll(eventBus);
     }
