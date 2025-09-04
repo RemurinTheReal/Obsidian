770e269d50602c4d99c14a49218299c3ed35843c
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-09-04 18:06:27 -0400

    Remove missing mixin

--- neoforge/src/main/java/com/remurinthereal/obsidian/api/data/ExtendedLanguageProvider.java
+++ neoforge/src/main/java/com/remurinthereal/obsidian/api/data/ExtendedLanguageProvider.java
@@ -1,9 +1,6 @@
 package com.remurinthereal.obsidian.api.data;
 
-import net.minecraft.Util;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.JukeboxSong;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 
 public abstract class ExtendedLanguageProvider extends LanguageProvider {
